30fc28ab0fc1d710dcb70cba665f31333445e9d1
Gump Over <overgump@hotmail.com>
2023-03-25 12:08:20 +1000

    сайт готов, надо переделовать

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,15 +1,25 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using System.Data;
+﻿using Microsoft.AspNetCore.Mvc;
+using MyCompany.Domain;
 
 namespace MyCompany.Controllers
 {
     public class HomeController : Controller
     {
-        
+        private readonly DataManager dataManager;
+
+        public HomeController(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageIndex"));
+        }
+
+        public IActionResult Contacts()
+        {
+            return View(dataManager.TextFields.GetTextFieldByCodeWord("PageContacts"));
         }
     }
 }
---
+++ Views/Shared/Components/Sidebar/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyCompany.Views.Shared.Components.Sidebar
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
