f24cf65b1d1c9197f5c42ac1aa3b877563577955
Gump Over <overgump@hotmail.com>
2023-03-27 14:49:00 +1000

    какая то хуйня

--- Startup.cs
+++ Startup.cs
@@ -59,7 +59,7 @@ namespace MyCompany
             });
 
             //добавляем сервисы для контроллеров и представлений (MVC)
-            services.AddControllersWithViews(x =>
+            _ = services.AddControllersWithViews(x =>
             {
                 x.Conventions.Add(new AdminAreaAuthorization("Admin", "AdminArea"));
             })
